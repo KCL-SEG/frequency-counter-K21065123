692cfe24930bc0a85850384bdfaccd203734d608
Joori <Joori.Almusaed@kcl.ac.uk>
2023-10-01 03:55:04 +0100

    Solution for Frequency counter exercise

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,13 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
-    return frequencies
+    
+    for i in items:
+        str_items = [str(i) for i in items]
+
+    
+    for i in str_items:
+        c = str_items.count(i)
+        frequencies[i]= c
+
+    return frequencies 
